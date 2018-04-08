f3d959e503972ca8c41a9b32bc7aa694687ce81e
chenjibao <17327364980@163.com>
2018-04-08 11:08:22 +0800

    后台一级分类管理查询所有

---
+++ src/cjb/shop/category/adminaction/AdminCategoryAction.java
@@ -0,0 +1,37 @@
+package cjb.shop.category.adminaction;
+
+import java.util.List;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+import com.opensymphony.xwork2.ModelDriven;
+
+import cjb.shop.category.domain.Category;
+import cjb.shop.category.service.CategoryService;
+/**
+ *@author chenjibao
+ *@date2018年4月8日上午10:45:39
+ *@description:后台一级分类管理的Action
+ */
+public class AdminCategoryAction extends ActionSupport implements ModelDriven<Category>{
+	//模型
+	private Category category=new Category();
+	@Override
+	public Category getModel() {
+		return category;
+	}
+	//注入一级分类的service
+	private CategoryService categoryService;
+	public void setCategoryService(CategoryService categoryService) {
+		this.categoryService = categoryService;
+	}
+	
+	//查询所有一级分类
+	public String findAll(){
+		List<Category> cList=categoryService.findAll();
+		//将集合保存到值栈中
+		ActionContext.getContext().getValueStack().set("cList", cList);
+		return "findAll";
+	}
+
+}
